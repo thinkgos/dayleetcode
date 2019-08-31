7cc5a25f29b870989c705d6c26267b6f412a40f7
mo <thinkgo@aliyun.com>
2019-08-31 16:28:50 +0800

    增加时间计算 好像不太准！！！

--- algorithm/191bitcount/bitcount.py
+++ algorithm/191bitcount/bitcount.py
@@ -1,6 +1,11 @@
 import unittest
+import time
 
-bits = [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,  # 0
+
+
+
+class Solution():
+    bits = [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,  # 0
         1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,  # 1
         1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,  # 1
         2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,  # 2
@@ -16,8 +21,18 @@ bits = [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,  # 0
         3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,  # 3
         3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,  # 3
         4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8]  # 4
+    
+    def wasteTime(func):
+        def wrapper(self, *args):
+            mark = time.time()
+            result = func(self, *args)
+            now = time.time()
+            print("waste time: {:.4} s".format((now - mark)))
+            return result
 
-class Solution():
+        return wrapper
+
+    @wasteTime
     def hammingWeight(self, n):
         """
         暴力解决法
@@ -31,6 +46,7 @@ class Solution():
             n >>= 1
         return cnt
 
+    @wasteTime
     def hammingWeight1(self, n):
         """
         借位,主要利用2进制,向高位借位,而把1计算出来
@@ -46,25 +62,39 @@ class Solution():
             n = n & (n - 1)
         return cnt
 
+    @wasteTime
     def hammingWeight2(self, n):
         """
         空间换时间了,列出8位所有的位表,应该是最快的
         :type n: int
         :rtype: int
         """
-        return bits[n&0xff] + bits[(n >> 8)&0xff] + bits[(n >> 16)&0xff] + bits[(n >> 24)&0xff]
+        return self.bits[n & 0xff] + self.bits[(n >> 8) & 0xff] + self.bits[(n >> 16) & 0xff] + self.bits[(n >> 24) & 0xff]
 
+    @wasteTime
+    def hammingWeight3(self, n):
+        """
+        leetcode上看的,比上面还快,全内置函数！！！！
+        :type n: int
+        :rtype: int
+        """
+        s = bin(n)[2:]
+        return s.count("1")
 
 class TestSolution(unittest.TestCase):
     def setUp(self) -> None:
         self.obj = Solution()
+
     def test_hammingWeight(self):
-        self.assertEqual(self.obj.hammingWeight(0x55),4)
-        self.assertEqual(self.obj.hammingWeight1(0x55),4)
-        self.assertEqual(self.obj.hammingWeight2(0x55),4)
-        self.assertEqual(self.obj.hammingWeight(0x8f),5)
-        self.assertEqual(self.obj.hammingWeight1(0x8f),5)
-        self.assertEqual(self.obj.hammingWeight2(0x8f),5)
+        self.assertEqual(self.obj.hammingWeight(0x55), 4)
+        self.assertEqual(self.obj.hammingWeight(0x8f), 5)
+        self.assertEqual(self.obj.hammingWeight1(0x55), 4)
+        self.assertEqual(self.obj.hammingWeight1(0x8f), 5)
+        self.assertEqual(self.obj.hammingWeight2(0x55), 4)
+        self.assertEqual(self.obj.hammingWeight2(0x8f), 5)
+        self.assertEqual(self.obj.hammingWeight3(0x55), 4)
+        self.assertEqual(self.obj.hammingWeight3(0x8f), 5)
+
 
 if __name__ == '__main__':
     unittest.main()
