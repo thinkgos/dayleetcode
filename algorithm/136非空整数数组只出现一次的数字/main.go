b48ef3e3a5e51ead9a3137a81c3f80a297111874
mo <thinkgo@aliyun.com>
2020-06-23 10:13:29 +0800

    增加找出只出现一次数字的数

---
+++ algorithm/136非空整数数组只出现一次的数字/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/google/go-cmp/cmp"
+)
+
+func main() {
+	nums := []int{1, 2, 2, 1, 4, 5, 5}
+
+	if !cmp.Equal(singleNumber(nums), 4) {
+		panic("singleNumber not equal")
+	}
+
+	if !cmp.Equal(singleNumber2(nums), 4) {
+		panic("singleNumber2 not equal")
+	}
+}
+
+// 暴力法,hash实现,使用额外空间
+func singleNumber(nums []int) int {
+	m := make(map[int]int)
+	for _, v := range nums {
+		if _, exist := m[v]; exist {
+			delete(m, v)
+		} else {
+			m[v] = 1
+		}
+	}
+	for k := range m {
+		return k
+	}
+	return 0
+}
+
+// 异域法,牛逼,没想到
+func singleNumber2(nums []int) int {
+	res := 0
+	for _, v := range nums {
+		res ^= v
+	}
+	return res
+}
