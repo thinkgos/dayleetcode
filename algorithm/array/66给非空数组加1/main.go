cc1bc3a51dc70182dc5a83b3210b3b7a327fcfb7
mo <thinkgo@aliyun.com>
2020-07-10 13:55:29 +0800

    move fix add one

--- algorithm/array/66给非空数组加1/main.go
+++ algorithm/array/66给非空数组加1/main.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("%#v\r\n", plusOne([]int{1, 9, 8}))
+}
+
 func plusOne(digits []int) []int {
-	for i := len(digits); i >= 0; i-- {
+	for i := len(digits) - 1; i >= 0; i-- {
 		if digits[i] < 9 { // 无需进位
 			digits[i]++
 			return digits
